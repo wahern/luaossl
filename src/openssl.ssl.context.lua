c80559d64791e42f8fec9056343f4629720323e4
daurnimator <quae@daurnimator.com>
2015-11-15 20:31:47 +1100

    Fix not passing through first cipher argument in vararg form

--- src/openssl.ssl.context.lua
+++ src/openssl.ssl.context.lua
@@ -5,7 +5,7 @@ local pack = table.pack or function(...) return { n = select("#", ...); ... } en
 -- Allow passing a vararg of ciphers, or an array
 local setCipherList; setCipherList = ctx.interpose("setCipherList", function (self, ciphers, ...)
 	if (...) then
-		local ciphers_t = pack(...)
+		local ciphers_t = pack(ciphers, ...)
 		ciphers = table.concat(ciphers_t, ":", 1, ciphers_t.n)
 	elseif type(ciphers) == "table" then
 		ciphers = table.concat(ciphers, ":")
