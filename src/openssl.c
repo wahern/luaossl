e632369c52cbfa14f62e70838a48f586a82c0230
daurnimator <quae@daurnimator.com>
2018-03-27 15:15:02 +1100

    src/openssl.c: openssl 1.1.1 has SSL_CTX_set1_cert_store
    Added in https://github.com/openssl/openssl/pull/1755

--- src/openssl.c
+++ src/openssl.c
@@ -298,7 +298,7 @@
 #endif
 
 #ifndef HAVE_SSL_CTX_SET1_CERT_STORE
-#define HAVE_SSL_CTX_SET1_CERT_STORE (HAVE_SSL_CTX_set1_cert_store || 0) /* backwards compatible with old macro name */
+#define HAVE_SSL_CTX_SET1_CERT_STORE (HAVE_SSL_CTX_set1_cert_store || OPENSSL_PREREQ(1,1,1)) /* backwards compatible with old macro name */
 #endif
 
 #ifndef HAVE_SSL_CTX_SET1_PARAM
